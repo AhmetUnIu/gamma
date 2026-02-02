a6bb66b478d46744b595e82e78dcbd7846f8f425
AhmetUnIu <ahmmetunlu@gmail.com>
2026-02-02 21:11:23 +0300

    Update Program2.py
    Added logic to check if a list is a palindrome.

--- oop/palindrome/Program2.py
+++ oop/palindrome/Program2.py
@@ -1,1 +1,25 @@
+list1 = [1, 5, 4, 9, 0]
 
+# for i in meetings[::-1]:
+#     print(i)
+
+list2 = []
+reverse_list = []
+
+for i in range(len(list1)):
+    # print(meetings[i], end = " ")
+    list2.append(list1[i])
+
+
+for i in range(len(list1) - 1, -1, -1):
+    # print(meetings[i], end = " ")
+    reverse_list.append(list1[i])
+
+print(list2)
+print(reverse_list)
+
+
+if list2 == reverse_list:
+    print("\nit is a polidron")
+else:
+    print("\nit is not a polidron")
