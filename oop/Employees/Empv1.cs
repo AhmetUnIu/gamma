a6c449b1b46582e5fb9c57a4fed17ce2f37604eb
AhmetUnIu <ahmmetunlu@gmail.com>
2026-02-03 00:25:53 +0300

    Create Empv1.cs

---
+++ oop/Employees/Empv1.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace oop
+{   
+    public class Employee
+    {
+        public string name;
+        public string position;
+        public double salary;
+        public int age;
+        
+        static int getNum()
+        {
+           Random random = new Random();
+           int num;
+           num = random.Next(18, 78);
+           return num;
+        }
+        
+        
+        public Employee(string nm, string pos, double sr)
+        {
+            
+            name = nm;
+            position = pos;
+            salary = sr;
+            age = getNum();
+        }
+    }
+    
+    class Program
+    {   
+        
+        public static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Ahmet", "Student", 2345);
+            Console.WriteLine("{0} {1} {2} {3}", emp1.name, emp1.position, emp1.salary, emp1.age);
+        }
+    }
+}
